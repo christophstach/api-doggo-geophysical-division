5c837eedd71ed393a100d8d9a59a117c20906ad1
Christoph Stach <christoph.stach@gmail.com>
2017-11-21 17:09:57 -0600

    mails are working now

---
+++ resources/views/emails/email_form_used.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <h1>Mail from christophstach.me</h1>
+
+    <hr>
+
+    <div>{{ $theEmailFrom }}</div>
+    <div>{{ $theEmailSubject }}</div>
+    <div>{{ $theEmailMessage }}</div>
+</div>
